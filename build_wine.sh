16943ab14ed0d23cff51c8b177342a74412f16e9
ewt45 <79033456+ewt45@users.noreply.github.com>
2024-08-17 21:46:55 +0800

    Update build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -62,7 +62,7 @@ export EXPERIMENTAL_WOW64="${EXPERIMENTAL_WOW64:-false}"
 #
 # If you don't want to compile a custom Wine source code, then just leave this
 # variable empty.
-export CUSTOM_SRC_PATH="https://github.com/ewt45/wine-fork"
+export CUSTOM_SRC_PATH=""
 
 # Set to true to download and prepare the source code, but do not compile it.
 # If this variable is set to true, root rights are not required.
